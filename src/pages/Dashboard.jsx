3f7d202878b28d108399e10ba5483542fbffd440
Gaurav kumar <01itsgauravpal@gmail.com>
2026-02-12 11:23:33 +0530

    Implement  the total user and total active user summary card logic

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import { useAppContext } from "../context/AppProvider";
+
 export default function Dashboard(){
+
+    const { users, requests, currentRole } = useAppContext();
+    
+    const activeUsersCount = users.filter((user) => user.status === "Active").length;
+    
 
     return(
         <div>
-            <h1>Your are in dasboard</h1>
+            {currentRole === "admin" 
+            ? 
+                <>
+                    <div>
+                        <div>{`Total users are : ${users.length}`}</div>
+                        <div>{`Total active user : ${activeUsersCount}`}</div>
+                    </div>
+                </>
+            :<><p>You are in member dashbaord</p></>}
         </div>
     )
 }
