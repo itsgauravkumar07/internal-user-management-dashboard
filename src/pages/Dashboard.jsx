9dee598e14049b5dd08bc688ce72e7a9d065c021
Gaurav kumar <01itsgauravpal@gmail.com>
2026-02-12 11:32:56 +0530

    Implement the total admin count and totaal pending request summary card logic

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,8 +6,8 @@ export default function Dashboard(){
     const { users, requests, currentRole } = useAppContext();
     
     const activeUsersCount = users.filter((user) => user.status === "Active").length;
-    
-
+    const adminCount = users.filter((user) => user.role === "admin").length;
+    const pendingReqCount = requests.filter((req) => req.status === "pending").length;
     return(
         <div>
             {currentRole === "admin" 
@@ -16,9 +16,17 @@ export default function Dashboard(){
                     <div>
                         <div>{`Total users are : ${users.length}`}</div>
                         <div>{`Total active user : ${activeUsersCount}`}</div>
+                        <div>{`Toatl admin count : ${adminCount}`}</div>
+                        <div>{`Toatl pending request : ${pendingReqCount}`}</div>
                     </div>
                 </>
-            :<><p>You are in member dashbaord</p></>}
+            : 
+                <>
+                    <div>
+                        <div>{}</div>
+                    </div>
+                </>
+            }
         </div>
     )
 }
