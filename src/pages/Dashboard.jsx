738d455a42d7bac690a9446f28e5ed005529b219
Gaurav kumar <01itsgauravpal@gmail.com>
2026-02-12 12:23:11 +0530

    Implement the summary card logic of current role and current status on  members dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,14 @@ import { useAppContext } from "../context/AppProvider";
 
 export default function Dashboard(){
 
-    const { users, requests, currentRole } = useAppContext();
+    const { users, requests, currentRole, currentUserId } = useAppContext();
     
     const activeUsersCount = users.filter((user) => user.status === "Active").length;
     const adminCount = users.filter((user) => user.role === "admin").length;
     const pendingReqCount = requests.filter((req) => req.status === "pending").length;
+    
+    const currentUser = users.find(u => u.id === currentUserId);
+
     return(
         <div>
             {currentRole === "admin" 
@@ -16,14 +19,16 @@ export default function Dashboard(){
                     <div>
                         <div>{`Total users are : ${users.length}`}</div>
                         <div>{`Total active user : ${activeUsersCount}`}</div>
-                        <div>{`Toatl admin count : ${adminCount}`}</div>
-                        <div>{`Toatl pending request : ${pendingReqCount}`}</div>
+                        <div>{`Total admin count : ${adminCount}`}</div>
+                        <div>{`Total pending request : ${pendingReqCount}`}</div>
                     </div>
                 </>
             : 
                 <>
                     <div>
-                        <div>{}</div>
+                        <div>{`Your role is : ${currentUser?.role}`}</div>
+                        <div>{`Your Current status is : ${currentUser?.status}`}</div>
+                        
                     </div>
                 </>
             }
