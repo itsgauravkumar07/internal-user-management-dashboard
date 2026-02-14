690c527b38d9d78dbe5524d5219caa82388fa55e
Gaurav kumar <01itsgauravpal@gmail.com>
2026-02-14 16:22:23 +0530

    Decrease the padding of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     const { currentRole } = useAppContext();
 
     return (
-        <nav className='flex justify-between bg-slate-700 items-center py-5 px-5 '>
+        <nav className='flex justify-between bg-slate-700 items-center py-3 px-5 '>
             <h1 className='font-bold text-2xl text-white'>Internal Dashboard</h1>
             <p className='text-sm px-4 py-2 bg-blue-800/60 text-white font-medium rounded-lg border border-blue-600/40'>{ currentRole }</p>
         </nav>
