36c5fe88376e39c6337c8b7a30b31441f1016991
Gaurav kumar <01itsgauravpal@gmail.com>
2026-02-14 16:03:07 +0530

    Polish the UI of the selectRole screen

--- src/pages/RoleSelect.jsx
+++ src/pages/RoleSelect.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppProvider"
+import { MdOutlineSecurity } from "react-icons/md";
+import { MdOutlinePerson2 } from "react-icons/md";
 
 export default function RoleSelect(){
 
@@ -18,27 +20,40 @@ export default function RoleSelect(){
         navigate("/dashboard")
     }
     return(
-        <section  className=" flex justify-center items-center px-10 flex-col min-h-screen gap-5 bg-slate-900">
+        <section  className="max-w-8xl flex justify-center items-center px-10 flex-col min-h-screen bg-slate-900">
 
-            <div className="text-center max-w-2xl space-y-6 mb-10">
-                <p className="text-blue-800 font-bold">DASHBOARD DEMO</p>
-                <h1 className="text-5xl font-black text-white">Select a Role to Preview</h1>
-                <p className="text-slate-300 text-2xl">Experience the dashboard from different perspectives. Choose a role below to enter the demo environment.</p>
+            <div className="text-center max-w-2xl space-y-4 mb-10 mt-10">
+                <p className="text-blue-800 font-bold text-sm">DASHBOARD DEMO</p>
+                <h1 className="text-4xl md:text-5xl font-black text-white">Select a Role to Preview</h1>
+                <p className="text-slate-400 text-lg font-medium">Experience the dashboard from different perspectives. Choose a role below to enter the demo environment.</p>
             </div>
-            <div className="flex justify-center items-center gap-1 md:flex-col w-full">
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-5xl mb-10">
+                {/* Card 01 */}
                 <div 
-                className="border border-gray-500 px-10 py-10 rounded-lg h-80 w-full"
-                onClick={setAdmin}
-                >
-                <h1>View as admin</h1>
-            </div>
+                    className="border border-slate-400/20 px-8 py-5 rounded-2xl bg-slate-800/80 hover:border-blue-500/50"
+                    onClick={setAdmin}>
+                    <div className="bg-blue-800/20 w-fit rounded-2xl py-4 px-3 mb-5 ">
+                        <MdOutlineSecurity className="h-10 w-10 text-blue-500/70"/>
+                    </div>
+                    <h1 className="text-2xl font-bold text-white mb-5">View as Admin</h1>
+                    <p className="text-slate-300  mb-5">Full access to all system settings. Manage users, configure permissions, and oversee access requests across the organizations.</p>
+                    <p className="text-blue-600 font-bold mb-2 text-sm">Enter as Admin</p>
+                </div>
+                
 
-             <div 
-                className="border border-gray-500 px-10 py-10 rounded"
-                onClick={setMember}
-                >
-                <h1>View as Member</h1>
-            </div>
+                {/* Card 02 */}
+                <div 
+                    className="border border-slate-400/20 px-8 py-5 rounded-2xl bg-slate-800/80 hover:border-blue-500/50"
+                    onClick={setMember}>
+                    <div className="bg-blue-800/20 w-fit rounded-2xl py-4 px-3 mb-5 ">
+                        <MdOutlinePerson2 className="h-10 w-10 text-blue-500/70"/>
+                    </div>
+                    <h1 className="text-2xl font-bold text-white mb-5">View as Member</h1>
+                    <p className="text-slate-300 mb-5">Standard user access. View directory, update personal profile, and submit requests for new resource access.</p>
+                    <p className="text-blue-600 font-bold mb-2 text-sm">Enter as Member</p>
+                </div>
+                
             </div>
             
         </section>
